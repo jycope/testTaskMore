6d4417b7584138f274ffd75c88da14e641ee1d46
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-08-14 19:39:00 +0500

    Верста левой части

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<aside>
+    <b>Категория 1</b>
+    <ul class="categories">
+        <li class="categories__item">Подкатегория 1-1</li>
+        <li class="categories__item">Подкатегория 1-2</li>
+    </ul>
+</aside>
+
+@endsection
