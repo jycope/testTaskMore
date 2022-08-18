fa8b49d5a38d3ff7e7f5ebe2fac7eaa5f471c13b
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-08-18 23:12:29 +0500

    Рефакторинг показа страницы продуктов

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -3,19 +3,19 @@
 @section('content')
 <main class="category-content">
     <h1 class="category-content__title">{{$category->name}}</h1>
-    <sub class="category-content__subtitle">Категория 2 / Подкатегория 2-1</sub>
-    <a href="{{ route('categories.product.create', $category) }}" class="d-block btn btn-primary mt-2">Создать
+    <sub class="category-content__subtitle d-block">Категория 2 / Подкатегория 2-1</sub>
+    <a href="{{ route('categories.product.create', $category) }}" class="btn btn-primary mt-2">Создать
         товар</a>
     <div class="category-content__content">
-
         @foreach ($products as $product)
-        <div class="category-content__item category-content-item">
+        <a href="{{ route('categories.product.show', [$category, $product]) }}"
+            class="category-content__item category-content-item">
             <img src="{{ asset("/storage/uploads" . $product->img) }}" class="category-content-item__image" alt="">
             <div class="category-content-item__name-and-price d-flex justify-content-between">
                 <b class="category-content-item__name">{{$product->name}}</b>
-                <span class="category-content-item__price">{{$product->price}}</span>
+                <span class="category-content-item__price">{{$product->price}} руб</span>
             </div>
-        </div>
+        </a>
         @endforeach
     </div>
 </main>
