9f58e87cb3e2a41821eada8c15fd2a9a542ae666
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-08-18 21:53:33 +0500

    Обернул продукт в класс category-content__item

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -2,20 +2,21 @@
 
 @section('content')
 <main class="category-content">
-    <h1 class="category-content__title">Подкатегория 2-1</h1>
+    <h1 class="category-content__title">{{$category->name}}</h1>
     <sub class="category-content__subtitle">Категория 2 / Подкатегория 2-1</sub>
     <a href="{{ route('categories.product.create', $category) }}" class="d-block btn btn-primary mt-2">Создать
         товар</a>
     <div class="category-content__content">
+
+        @foreach ($products as $product)
         <div class="category-content__item category-content-item">
-            @foreach ($products as $product)
             <img src="{{ asset("/storage/uploads" . $product->img) }}" class="category-content-item__image" alt="">
             <div class="category-content-item__name-and-price d-flex justify-content-between">
                 <b class="category-content-item__name">{{$product->name}}</b>
                 <span class="category-content-item__price">{{$product->price}}</span>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
 </main>
 
