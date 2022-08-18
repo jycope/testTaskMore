b83ffeec5b5f17148b582254641e210ecb114bd9
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-08-18 18:17:21 +0500

    ProductController с роидетелем Category

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,107 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function show($cat, $product_id)
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Category $category)
     {
+        //
     }
 
-    public function showCategory()
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Category $category)
     {
+        $categories = Category::whereNull('category_id')
+            ->with('childrenCategories')
+            ->get();
+        $product = new Product();
+
+        return view('product.create', compact('categories', 'product', 'category'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Category $category)
+    {
+        $data = $this->validate($request, [
+            'price' => 'required|integer',
+            'img' => 'required',
+            'name' => 'required',
+            'category_id' => 'required'
+        ]);
+        $product = new Product();
+        $product->fill($data);
+        $product->save();
+        return redirect()
+            ->route('categories.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category, Product $product)
+    {
+        //
     }
 }
