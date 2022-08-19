ef2ec088ad84c44b888ae26934af94f3c3504878
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-08-19 16:58:06 +0500

    форма создания категории

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -12,42 +12,15 @@
 </div>
 @endif
 
-{{ Form::model($worker, ['route' => 'staff.store', 'class' => 'd-flex justify-content-between flex-column gap-10 mt-4 w-25 m-auto', 'enctype' 
-<div class="mb-1">
-    <label for="staticEmail" class="col-form-label">Email</label>
-    <div class="input-form">
-        {{ Form::text('email') }}
-</div>
-</div>
+{{ Form::model($category, ['route' => 'categories.store', 'class' => 'd-flex justify-content-between flex-column gap-10 mt-4 m-auto'], 'enctype') }}
 
 <div class="mb-2">
-    <label for="staticEmail" class="col-form-label">ФИО</label>
+    <label for="staticEmail" class="col-form-label">Название</label>
     <div class="input-form">
-        {{ Form::text('fio') }}
+        {{ Form::text('name', '', ['placeholder' => 'Кухни...']) }}
     </div>
 </div>
 
-<div class="mb-2">
-    <label for="staticEmail" class="col-form-label">Опыт работы</label>
-    <div class="input-form">
-        {{ Form::text('work_experience') }}
-    </div>
-</div>
-
-<div class="mb-2">
-    <label for="staticEmail" class="col-form-label">Фото</label>
-    {{csrf_field()}}
-    <div class="input-form">
-        {{ Form::file('photo') }}
-    </div>
-</div>
-
-<div class="mb-2">
-    <label for="staticEmail" class="col-form-label">Средняя З/П</label>
-    <div class="input-form">
-        {{ Form::number('average_salary') }}
-    </div>
-</div>
 {{ Form::submit('Добавить', ['class' => 'form-control mb-4 btn btn-primary']) }}
 {{ Form::close() }}
 
