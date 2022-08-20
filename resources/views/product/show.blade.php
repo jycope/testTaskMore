1b3250c26c2b318dede53ae8961083c8513ffb86
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-08-21 00:00:06 +0500

    Верстка удаления и редактирования в шаблоне

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -13,6 +13,18 @@
         </div>
         <p class="product__description">{{ $product->description }}</p>
     </div>
+    <div class="d-flex justify-content-between">
+        <a href="{{ route('categories.product.edit', [$category, $product]) }}"
+            class="btn btn-primary mt-2 d-block">Редактировать
+            товар</a>
+
+        <form action="{{ route('categories.product.destroy', [$category, $product]) }}" method="post">
+            <input type="hidden" name="_method" value="DELETE">
+            <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+            <button type="submit" class="btn btn-primary mt-2"
+                onClick="return confirm('Вы подтверждаете удаление?');">Удалить товар</button>
+        </form>
+    </div>
 </div>
 
 @endsection
